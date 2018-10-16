ba3cfac9bf6ebfe6a5382d05e738bc728403d7d6
Sharmina Akhtar <sakht318@gmail.com>
2018-10-16 18:05:21 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,11 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+
+export default class ThemedDecoration extends React.Component {
+  render(){
+    return (
+      <h1>{"You've been invited!"}</h1>
+      {this.props.children}
+    )
+  }
+}

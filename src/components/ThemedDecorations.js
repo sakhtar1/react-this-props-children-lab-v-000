c8a482ce56c4497fac4a6a42255e627cb23408f4
Sharmina Akhtar <sakht318@gmail.com>
2018-10-16 18:05:33 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -5,7 +5,7 @@ export default class ThemedDecoration extends React.Component {
   render(){
     return (
       <h1>{"You've been invited!"}</h1>
-      {this.props.children}
+        {this.props.children}
     )
   }
 }
